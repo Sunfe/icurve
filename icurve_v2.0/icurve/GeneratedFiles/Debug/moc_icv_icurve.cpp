a5d3166fa2ebb7eea54516f258750a5a369b37f7
sunfe <lzc721@gmail.com>
2016-06-23 19:23:58 +0800

    vc project setting changed.20160623-19:23

--- icurve_v2.0/icurve/GeneratedFiles/Debug/moc_icv_icurve.cpp
+++ icurve_v2.0/icurve/GeneratedFiles/Debug/moc_icv_icurve.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'icv_icurve.h'
 **
-** Created: Thu Jun 23 18:33:09 2016
+** Created: Thu Jun 23 19:19:01 2016
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.3)
 **
 ** WARNING! All changes made in this file will be lost!
--- icurve_v2.0/icurve/GeneratedFiles/ui_icv_marker_property.h
+++ icurve_v2.0/icurve/GeneratedFiles/ui_icv_marker_property.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'icv_marker_property.ui'
 **
-** Created: Thu Jun 23 18:33:08 2016
+** Created: Thu Jun 23 19:19:00 2016
 **      by: Qt User Interface Compiler version 4.7.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
